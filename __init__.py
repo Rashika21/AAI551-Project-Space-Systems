31ded8e45e0b6efb913c7879ea89138c9a76dd7e
rashikasn <64012053+Rashika21@users.noreply.github.com>
2025-12-17 22:33:04 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+Satellite Trajectory Predictor Package
+
+A Python-based tool for predicting and plotting satellite orbital trajectories.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Rashika Sugganahalli Natesh Babu, Behnam Nejati"
+
+
+
+
+
+
+
